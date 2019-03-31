4410f28f708591204ce974d679a63bef5d8ef62d
Alexander <ovcharenko0101sasha@gmail.com>
2019-03-31 09:09:48 +0300

    add

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+      $('.team-slider').slick({
+        infinite: true,
+        dots: false,
+        speed: 900,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		  ]
+      });
+    });
